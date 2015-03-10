38ff01feb33ae3dac319e370d41685d525b993f2
りぶ <g.s.sequence+github@gmail.com>
2015-03-10 15:14:48 +0900

    Add library 'MahApps.Metro'

--- CtrlLauncher/Views/MainWindow.xaml.cs
+++ CtrlLauncher/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,7 +26,7 @@ namespace CtrlLauncher.Views
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
