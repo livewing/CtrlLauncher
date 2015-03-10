51b8afdfe64726ac199216c4af5d12dea61593d3
りぶ <g.s.sequence@gmail.com>
2015-03-10 22:15:06 +0900

    Add about flyout

---
+++ CtrlLauncher/Models/About.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Livet;
+using System.IO;
+
+namespace CtrlLauncher.Models
+{
+    public class About : NotificationObject
+    {
+        public string Version { get; private set; }
+
+        public IEnumerable<Tuple<string, string, string>> Licenses { get; private set; }
+
+        public void OpenUri(string uriString)
+        {
+            Process.Start(uriString);
+        }
+
+        public About()
+        {
+            // バージョンを取得
+            Version = Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+
+            // 埋め込まれたライセンス条項ファイルの読み込み
+            var asm = Assembly.GetExecutingAssembly();
+            var licenses = asm.GetManifestResourceNames().Where(n => n.StartsWith("CtrlLauncher.Resources.Licenses."));
+            var list = new List<Tuple<string, string, string>>();
+            foreach (var lic in licenses)
+            {
+                using (var sr = new StreamReader(asm.GetManifestResourceStream(lic)))
+                {
+                    var name = sr.ReadLine();
+                    var url = sr.ReadLine();
+                    sr.ReadLine();
+                    var text = sr.ReadToEnd();
+                    list.Add(new Tuple<string, string, string>(name, url, text));
+                }
+            }
+            Licenses = list;
+        }
+    }
+}
--- CtrlLauncher/ViewModels/MainWindowViewModel.cs
+++ CtrlLauncher/ViewModels/MainWindowViewModel.cs
@@ -34,6 +34,46 @@ namespace CtrlLauncher.ViewModels
         }
         #endregion
 
+        #region IsOpenAboutFlyout変更通知プロパティ
+        private bool _IsOpenAboutFlyout = false;
+
+        public bool IsOpenAboutFlyout
+        {
+            get
+            { return _IsOpenAboutFlyout; }
+            set
+            { 
+                if (_IsOpenAboutFlyout == value)
+                    return;
+                _IsOpenAboutFlyout = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region OpenAboutFlyoutCommand
+        private ViewModelCommand _OpenAboutFlyoutCommand;
+
+        public ViewModelCommand OpenAboutFlyoutCommand
+        {
+            get
+            {
+                if (_OpenAboutFlyoutCommand == null)
+                {
+                    _OpenAboutFlyoutCommand = new ViewModelCommand(OpenAboutFlyout);
+                }
+                return _OpenAboutFlyoutCommand;
+            }
+        }
+
+        public void OpenAboutFlyout()
+        {
+            IsOpenAboutFlyout = true;
+        }
+        #endregion
+
+
         public void Initialize()
         {
         }
